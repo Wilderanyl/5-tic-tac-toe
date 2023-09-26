73d4d86605c22885f3c2562aeef4a34a5e415764
Wilderanyl <wilderanyl@gmail.com>
2023-09-26 15:15:09 +0700

    done

--- index.js
+++ index.js
@@ -1,62 +1,82 @@
 const X_CLASS = "x";
 const O_CLASS = "o";
-const WINNING_POSSIBLE  = [
-    [0, 1, 2], 
-    [3, 4 ,5],
-    [6, 7, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8]
+const WINNING_POSSIBLE = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
 ];
 const boxesElement = document.querySelectorAll("[data-box]");
 const boardElement = document.getElementById("board");
+const restartContainer = document.getElementById("restart");
+const restartText = document.getElementById("restart-text");
+const restartButton = document.getElementById("restart-button");
 let oturn = false;
 
-boxesElement.forEach((box ) => {
-    box.addEventListener("click", handleClick, {once: true});
-});
+startGame();
 
-function handleHover () {
-    boardElement.classList.remove(O_CLASS);
-    boardElement.classList.remove(X_CLASS);
-    if(oturn) {
-        boardElement.classList.add(O_CLASS);
-    } else if(!oturn) {
-        boardElement.classList.add(X_CLASS);
-    } else ;
+restartButton.addEventListener("click", startGame);
+
+function startGame() {
+  oturn = false;
+  boxesElement.forEach((box) => {
+    box.classList.remove(X_CLASS);
+    box.classList.remove(O_CLASS);
+    box.removeEventListener("click", handleClick);
+    box.addEventListener("click", handleClick, { once: true });
+  });
+  handleHover();
+  restartContainer.classList.remove("show");
 }
 
-function handleClick (e) {
-    const currentClass = oturn ? O_CLASS : X_CLASS;
-    const targetBox = e.target;
-    targetBox.classList.add(currentClass);
-    if(checkWin(currentClass)) {
-       if(currentClass == X_CLASS) {
-        console.log("X is the winner");
-       } else {
-        console.log("o is the winner");
-       }
-    } 
-    swapTurn();
-    handleHover();
+function handleHover() {
+  boardElement.classList.remove(O_CLASS);
+  boardElement.classList.remove(X_CLASS);
+  if (oturn) {
+    boardElement.classList.add(O_CLASS);
+  } else if (!oturn) {
+    boardElement.classList.add(X_CLASS);
+  } else;
 }
 
-function isDraw () {    
-    return boxesElement.every((a) => {
-        return a.classList.contains(X_CLASS) ||  a.classList.contains(O_CLASS) 
-    })
+function handleClick(e) {
+  const currentClass = oturn ? O_CLASS : X_CLASS;
+  const targetBox = e.target;
+  targetBox.classList.add(currentClass);
+  if (checkWin(currentClass)) {
+    if (currentClass == X_CLASS) {
+      restartContainer.classList.add("show");
+      restartText.innerText = "X is the winner!";
+    } else {
+      restartContainer.classList.add("show");
+      restartText.innerText = "O is the winner!";
+    }
+  } else if (isDraw()) {
+    restartContainer.classList.add("show");
+    restartText.innerText = " Both are draw!";
+  }
+  swapTurn();
+  handleHover();
 }
 
-function swapTurn () {
-    oturn = !oturn;
+function isDraw() {
+  return [...boxesElement].every(
+    (a) => a.classList.contains(X_CLASS) || a.classList.contains(O_CLASS)
+  );
 }
 
-function checkWin (currentClass) {
-    return WINNING_POSSIBLE.some((a) => {
-        return a.every((i) => {
-            return boxesElement[i].classList.contains(currentClass);
-        })
-    })
+function swapTurn() {
+  oturn = !oturn;
+}
+
+function checkWin(currentClass) {
+  return WINNING_POSSIBLE.some((a) => {
+    return a.every((i) => {
+      return boxesElement[i].classList.contains(currentClass);
+    });
+  });
 }
